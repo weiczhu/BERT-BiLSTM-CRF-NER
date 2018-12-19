d65faf17ece9e9f36ac735f9aaa6a8f33446574b
weicheng.zhu(travis) <weicheng.zhu@rakuten.com>
2018-12-19 17:13:52 +0800

    Add dataset

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@ python3 bert_lstm_ner.py   \
                   --do_train=True   \
                   --do_eval=True   \
                   --do_predict=True \
-                  --data_dir=NERdata\ja   \
+                  --data_dir=NERdata/ja   \
                   --vocab_file=multi_cased_L-12_H-768_A-12/vocab.txt  \
                   --bert_config_file=multi_cased_L-12_H-768_A-12/bert_config.json \
                   --init_checkpoint=multi_cased_L-12_H-768_A-12/bert_model.ckpt   \
                   --max_seq_length=32   \
                   --train_batch_size=16   \
                   --learning_rate=2e-5   \
-                  --num_train_epochs=3.0   \
+                  --num_train_epochs=3   \
                   --output_dir=./output/result_dir/
